aadea0a628659aee5f40e7598a635d1d68432205
Hafidh Muqsithanova S <hafidhmn@gmail.com>
2025-01-05 15:59:02 +0700

    fix: fix UI

--- src/lib/components/TextPanel.svelte
+++ src/lib/components/TextPanel.svelte
@@ -12,14 +12,14 @@
 </script>
 
 <section class="flex h-full flex-col">
-	<header class="flex items-center gap-4 bg-red-700 px-6 py-1 text-red-100">
+	<header class="flex items-center gap-4 bg-red-700 px-2 lg:px-6 py-1 text-red-100">
 		<button class="rounded p-2 transition-colors hover:bg-red-600">
 			<Menu />
 		</button>
 		<h2 class="text-xl font-bold">Tour de Kaba</h2>
 	</header>
 
-	<div class="!w-full grow overflow-y-auto p-8">
+	<div class="!w-full grow overflow-y-auto p-4 lg:p-8">
 		<div
 			class="prose prose-neutral prose-invert prose-h1:text-2xl prose-h1:font-bold
 			prose-pre:border prose-pre:border-neutral-700 prose-pre:bg-neutral-900
@@ -29,7 +29,7 @@
 		</div>
 	</div>
 
-	<footer class="border-t border-neutral-700 px-8 py-2">
+	<footer class="border-t border-neutral-700 px-4 lg:px-8 py-2">
 		<TextPanelNavigation {prev} {next} />
 	</footer>
 </section>
