6bd9d97d58f85a0f0327f20d8e2cd3a06f518fe7
Hafidh Muqsithanova S <hafidhmn@gmail.com>
2025-01-06 11:29:08 +0700

    feat: add transition to navigation menu

--- src/lib/components/NavbarNavigation.svelte
+++ src/lib/components/NavbarNavigation.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { pages } from '$lib/constants/pages';
+	import { slide } from 'svelte/transition';
 
 	let { close }: { close: () => void } = $props();
 </script>
@@ -11,14 +12,15 @@
 ></div>
 
 <div
-	class="fixed inset-y-0 left-0 z-50 w-2/3 overflow-y-auto bg-neutral-800 p-8 text-neutral-300
+	transition:slide={{ axis: 'x' }}
+	class="overflow-x-hide fixed inset-y-0 left-0 z-50 w-2/3 overflow-y-auto bg-neutral-800 p-8 text-neutral-300
     md:w-1/2 lg:w-1/4"
 >
-	<h2 class="text-xl text-neutral-400">Table of Contents</h2>
+	<h2 class="text-nowrap text-xl text-neutral-400">Table of Contents</h2>
 
 	<nav class="mt-6 flex flex-col gap-4">
 		{#each pages as page, i (page.slug)}
-			<a href={page.slug} class="group flex items-center gap-2" onclick={close}>
+			<a href={page.slug} class="group flex items-center gap-2 text-nowrap" onclick={close}>
 				<span class="mr-1 w-4 text-neutral-500">{i}.</span>
 				<span class="transition-colors group-hover:text-red-500">
 					{page.title}
