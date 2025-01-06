d9e405589d421754c76ba636bb2637a93ba0b6eb
Hafidh Muqsithanova S <hafidhmn@gmail.com>
2025-01-06 12:08:37 +0700

    feat: add reset code button

--- src/lib/components/ResultPanel.svelte
+++ src/lib/components/ResultPanel.svelte
@@ -1,12 +1,21 @@
-<script>
+<script lang="ts">
 	import Play from 'lucide-svelte/icons/play';
+	import RotateCcw from 'lucide-svelte/icons/rotate-ccw';
 </script>
 
 <section class="absolute inset-x-0 bottom-0">
-	<header class="flex justify-end px-4 py-2 md:px-8">
+	<header class="flex justify-end gap-4 px-4 py-2 md:px-8">
+		<button
+			class="flex select-none items-center gap-2 rounded bg-neutral-600 px-4 py-2
+        text-neutral-200 transition-colors hover:bg-neutral-700"
+		>
+			<RotateCcw />
+			Reset
+		</button>
+
 		<button
 			class="flex select-none items-center gap-2 rounded bg-red-700 px-4 py-2
-        text-red-200 transition-colors hover:bg-red-800"
+        text-red-100 transition-colors hover:bg-red-800"
 		>
 			<Play />
 			Run
