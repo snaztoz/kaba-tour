e39b1a2d46fe5994b8247d54c352cdf521566e63
Hafidh Muqsithanova S <hafidhmn@gmail.com>
2025-01-06 15:49:55 +0700

    refactor: refactor theme

--- src/lib/components/CodePanel.svelte
+++ src/lib/components/CodePanel.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import CodeMirror from 'svelte-codemirror-editor';
-	import { EditorView } from '@codemirror/view';
+	import { theme } from '$lib/constants/editorTheme';
 
 	interface Props {
 		code: string;
@@ -8,36 +8,6 @@
 	}
 
 	let { code, update }: Props = $props();
-
-	const theme = EditorView.theme(
-		{
-			'&': {
-				height: '100%',
-				backgroundColor: '#171717',
-				fontSize: '15px',
-				color: '#a3a3a3'
-			},
-			'.cm-scroller': {
-				fontFamily:
-					'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
-				lineHeight: '24px',
-				overflowY: 'auto'
-			},
-			'.cm-gutters': {
-				backgroundColor: '#171717',
-				borderRightWidth: '1px',
-				borderColor: '#404040',
-				color: '#a3a3a3'
-			},
-			'& .cm-lineNumbers .cm-gutterElement': {
-				minWidth: '3rem',
-				paddingLeft: '0.75rem',
-				paddingRight: '0.75rem',
-				userSelect: 'none'
-			}
-		},
-		{ dark: true }
-	);
 </script>
 
 <section class="grow">
